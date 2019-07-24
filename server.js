f64f184538b7991409022d601edb591853751efe
Monika-Mistry <monika.mistry1@gmail.com>
2019-07-24 15:49:13 +0100

    mongo connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const item = require("./routes/item");
 
@@ -11,6 +12,13 @@ app.use(bodyParser.json());
 
 //Routes
 app.use("/item", item);
+
+//connect to database
+mongoose.connect('mongodb://localhost:27017/example',
+    { useNewUrlParser: true }
+).then(
+    () => { console.log("Connection Successful") },
+    err => { console.log(err.message) });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running on port ${port}`));
